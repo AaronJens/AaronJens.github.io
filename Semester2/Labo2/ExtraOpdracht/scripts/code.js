5377c1405b836ed9ecef413636e22e5b9a85944a
AaronVives <aaronjens.vandaele@student.vives.be>
2025-02-16 14:31:27 +0100

    toggle

--- Semester2/Labo2/ExtraOpdracht/scripts/code.js
+++ Semester2/Labo2/ExtraOpdracht/scripts/code.js
@@ -4,9 +4,10 @@ const setup = () => {
 
 const toggleList = () => {
     list.classList.toggle('modified');
+    list.classList.toggle('not-modified');
 };
 
-let button= document.getElementById('toggleButton');
-let list  = document.getElementById('myList');
+let button = document.getElementById('toggleButton');
+let list = document.getElementById('myList');
 
 window.addEventListener("load", setup);
